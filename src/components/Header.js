1aa599139077706c0c5cd2aefcee7ae0b612d428
eldray <83376852+eldray@users.noreply.github.com>
2023-11-09 09:50:40 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,39 @@
-// Header.js
-
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import logo from './images/logo.png';
 
 const Header = () => {
   return (
-    <header className="bg-primary p-3">
+    <header className="bg-light p-3">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-md-6">
-            <h1 className="text-white mb-0">NGO</h1>
+          <div className="col-md-4 d-flex justify-content-between align-items-center">
+            <a href="/">
+            <img src={logo} height={80} width={80} alt='logo' className="logo"/>
+            </a>
           </div>
-          <div className="col-md-6 text-md-end">
+          <div className="col-md-8 text-md-end d-flex justify-content-between">
             <nav>
-              <ul className="list-inline mb-0">
+              <ul className="list-inline mb-0 me-auto">
                 <li className="list-inline-item me-3">
-                  <Link to="/" className="text-white text-decoration-none">Home</Link>
+                  <Link to="/" className="text-decoration-none text-uppercase nav-link">Home</Link>
                 </li>
                 <li className="list-inline-item me-3">
-                  <Link to="/about" className="text-white text-decoration-none">About</Link>
+                  <Link to="/about" className="text-decoration-none text-uppercase nav-link">About</Link>
                 </li>
                 <li className="list-inline-item me-3">
-                  <Link to="/ministries" className="text-white text-decoration-none">Ministries</Link>
+                  <Link to="/ministries" className="text-decoration-none text-uppercase nav-link">Ministries</Link>
                 </li>
                 <li className="list-inline-item me-3">
-                  <Link to="/events" className="text-white text-decoration-none">Events</Link>
+                  <Link to="/events" className="text-decoration-none text-uppercase nav-link">Events</Link>
                 </li>
                 <li className="list-inline-item">
-                  <Link to="/donationform" className="text-white text-decoration-none">Donate</Link>
+                  <Link to="/contact" className="text-decoration-none text-uppercase nav-link">Contact</Link>
                 </li>
               </ul>
             </nav>
+            <Link to="/donationform" className="btn btn-success text-uppercase ms-3">Donate Now</Link>
           </div>
         </div>
       </div>
